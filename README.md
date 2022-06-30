259f98edbc431e2eeb1a30b447975c32525a417b
uzlopak <aras.abbasi@googlemail.com>
2022-06-30 09:50:18 +0200

    make bechmarks nicer

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![NPM version](https://img.shields.io/npm/v/@fastify/deepmerge.svg?style=flat)](https://www.npmjs.com/package/@fastify/deepmerge)
 [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://standardjs.com/)
 
-Merges the enumerable properties of two or more objects deeply.
+Merges the enumerable properties of two or more objects deeply. Fastest implementation of deepmerge, see section 'Benchmarks'.
 
 ### Install
 ```
@@ -34,6 +34,30 @@ console.log(result) // {a: 'value',  b: 404 }
 const deepmerge = require('@fastify/deepmegre')({ all: true })
 const result = deepmerge({a: 'value'}, { b: 404 }, { a: 404 })
 console.log(result) // {a: 404,  b: 404 }
+```
+
+## Benchmarks
+
+The benchmarks are available in the benchmark-folder. 
+
+`npm run bench` - benchmark various use cases of deepmerge:
+```
+@fastify/deepmerge: merge regex with date x 1,266,447,885 ops/sec ±0.14% (97 runs sampled)
+@fastify/deepmerge: merge object with a primitive x 1,266,435,016 ops/sec ±0.33% (97 runs sampled)
+@fastify/deepmerge: merge two arrays containing strings x 25,591,739 ops/sec ±0.24% (98 runs sampled)
+@fastify/deepmerge: two merge arrays containing objects x 976,182 ops/sec ±0.46% (98 runs sampled)
+@fastify/deepmerge: merge two flat objects x 10,027,879 ops/sec ±0.36% (94 runs sampled)
+@fastify/deepmerge: merge nested objects x 5,341,227 ops/sec ±0.67% (94 runs sampled)
+```
+
+`npm run bench:compare` - comparison of @fastify/deepmerge with other popular deepmerge implementation:
+```
+@fastify/deepmerge x 403,777 ops/sec ±0.22% (98 runs sampled)
+deepmerge x 21,143 ops/sec ±0.83% (93 runs sampled)
+merge-deep x 89,447 ops/sec ±0.59% (95 runs sampled)
+ts-deepmerge x 185,601 ops/sec ±0.59% (96 runs sampled)
+deepmerge-ts x 185,310 ops/sec ±0.50% (92 runs sampled)
+lodash.merge x 89,053 ops/sec ±0.37% (99 runs sampled)
 ```
 
 ## License
--- benchmark/bench.js
+++ benchmark/bench.js
@@ -3,10 +3,10 @@
 const Benchmark = require('benchmark')
 const deepmerge = require('..')({ symbol: false })
 
-const srcSimple = { key1: 'changed', key2: 'value2' }
+const sourceSimple = { key1: 'changed', key2: 'value2' }
 const targetSimple = { key1: 'value1', key3: 'value3' }
 
-const srcNested = {
+const sourceNested = {
   key1: {
     subkey1: 'subvalue1',
     subkey2: 'subvalue2'
@@ -25,27 +25,27 @@ const regex = /a/g
 const simpleArrayTarget = ['a1', 'a2', 'c1', 'f1', 'p1']
 const simpleArraySource = ['t1', 's1', 'c2', 'r1', 'p2', 'p3']
 
-const complexArraySource = [{ ...srcSimple }, { ...srcSimple }, { ...srcSimple }, { ...srcSimple }, { ...srcSimple }]
+const complexArraySource = [{ ...sourceSimple }, { ...sourceSimple }, { ...sourceSimple }, { ...sourceSimple }, { ...sourceSimple }]
 const complexArrayTarget = [{ ...targetSimple }, { ...targetSimple }, { ...targetSimple }, { ...targetSimple }, { ...targetSimple }]
 
 new Benchmark.Suite()
-  .add('deepmerge regex with date', function () {
+  .add('@fastify/deepmerge: merge regex with date', function () {
     deepmerge(regex, date)
   })
-  .add('deepmerge with primitive', function () {
+  .add('@fastify/deepmerge: merge object with a primitive', function () {
     deepmerge(targetSimple, primitive)
   })
-  .add('deepmerge simple Arrays', function () {
+  .add('@fastify/deepmerge: merge two arrays containing strings', function () {
     deepmerge(simpleArrayTarget, simpleArraySource)
   })
-  .add('deepmerge complex Arrays', function () {
+  .add('@fastify/deepmerge: two merge arrays containing objects', function () {
     deepmerge(complexArrayTarget, complexArraySource)
   })
-  .add('deepmerge existing simple keys in target at the roots', function () {
-    deepmerge(targetSimple, srcSimple)
+  .add('@fastify/deepmerge: merge two flat objects', function () {
+    deepmerge(targetSimple, sourceSimple)
   })
-  .add('deepmerge nested objects into target', function () {
-    deepmerge(targetNested, srcNested)
+  .add('@fastify/deepmerge: merge nested objects', function () {
+    deepmerge(targetNested, sourceNested)
   })
   .on('cycle', function (event) {
     console.log(String(event.target))
