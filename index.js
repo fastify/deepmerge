da0dbebcbb599bedf390ee9668a297efc9ff8a24
Frazer Smith <frazer.dev@icloud.com>
2025-01-09 19:20:45 +0000

    Update index.js
    Signed-off-by: Frazer Smith <frazer.dev@icloud.com>

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function deepmergeConstructor (options) {
     for (i; i < tl; ++i) {
       result[i] = clone(target[i])
     }
-    for (i; i < sl; ++i) {
+    for (i= 0; i < sl; ++i) {
       result[i + tl] = clone(source[i])
     }
     return result
